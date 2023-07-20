c729c61c42c40cc0a7bb4dc219182f44fa301239
[Unicatm] <[alexandra.tertiscu@gmail.com]>
2023-07-21 00:37:01 +0300

    wip

--- To-Do/list.js
+++ To-Do/list.js
@@ -1,26 +1,18 @@
 const addButton = document.getElementById("addItem");
-const removeButton = document.getElementById("removeItem");
+const removeButton = document.getElementById("trash");
 const undo = document.getElementById("undo");
 const list = document.getElementById("list");
+const inputBar = document.querySelector("#input-bar");
 
 const bgMove = document.querySelector(".container");
 const frontBlobs = document.querySelector(".front-blobs");
 const backBlobs = document.querySelector(".back-blobs");
 
 const count = list.children;
-console.log(count);
 let removedItems = [];
 
-addButton.addEventListener("click", function () {
-  const newItem = document.createElement("li");
-  newItem.classList.add("list");
-  newItem.innerText = `Item ${count.length + 1}`;
-  //newItem.innerText = prompt("Add text!");
-  list.appendChild(newItem);
-
-  //removing item
-  newItem.addEventListener("click", deleteItem);
-});
+addButton.addEventListener("click", addToDo);
+todo.addEventListener("click", deleteItem);
 
 // removeButton.addEventListener("click", function () {
 //   const lastElement = count[count.length - 1];
@@ -28,10 +20,36 @@ addButton.addEventListener("click", function () {
 //   removedItems.push(lastElement);
 //   console.log(removedItems);
 // });
+
+function addToDo(e) {
+  //Prevent the form from subbmiting
+  e.preventDefault();
+
+  const newTodoItem = document.createElement("div");
+  newTodoItem.classList.add("todo-item");
+
+  const checkBtn = document.createElement("button");
+  checkBtn.innerHTML = `<i class="fa-solid fa-check fa-lg"></i>`;
+  newTodoItem.appendChild(checkBtn);
+
+  const newItem = document.createElement("li");
+  newItem.classList.add("item");
+  newItem.innerText = `hei`;
+  newTodoItem.appendChild(newItem);
+
+  const trashBtn = document.createElement("button");
+  trashBtn.setAttribute("id", "trash");
+  trashBtn.innerHTML = `<i class="fa-regular fa-trash-can fa-lg"></i>`;
+  newTodoItem.appendChild(trashBtn);
+
+  list.appendChild(newTodoItem);
+}
+
 function deleteItem(e) {
-  console.log(e.target);
-  const removedItem = e.target.remove();
-  removedItems.push(removedItem);
+  // const deleteItem = document.getElementById("trash").closest("div");
+  // deleteItem.remove();
+  // console.log(deleteItem);
+  console.log(e);
 }
 
 undo.addEventListener("click", function () {
