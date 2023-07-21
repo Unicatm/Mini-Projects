5e4327a391db9a693a2e840ca7dea43df6a72d59
[Unicatm] <[alexandra.tertiscu@gmail.com]>
2023-07-21 11:22:35 +0300

    deleteBtn

--- To-Do/list.js
+++ To-Do/list.js
@@ -1,32 +1,22 @@
-const addButton = document.getElementById("addItem");
-const removeButton = document.getElementById("trash");
+const addButton = document.querySelector("#addItem");
 const undo = document.getElementById("undo");
-const list = document.getElementById("list");
-const inputBar = document.querySelector("#input-bar");
 
-const bgMove = document.querySelector(".container");
-const frontBlobs = document.querySelector(".front-blobs");
-const backBlobs = document.querySelector(".back-blobs");
+const task = document.querySelector("#task");
+const list = document.querySelector("#list");
+const inputBar = document.querySelector("#input-bar");
 
 const count = list.children;
 let removedItems = [];
 
 addButton.addEventListener("click", addToDo);
-todo.addEventListener("click", deleteItem);
-
-// removeButton.addEventListener("click", function () {
-//   const lastElement = count[count.length - 1];
-//   lastElement.remove();
-//   removedItems.push(lastElement);
-//   console.log(removedItems);
-// });
+list.addEventListener("click", deleteItem);
 
 function addToDo(e) {
   //Prevent the form from subbmiting
   e.preventDefault();
 
   const newTodoItem = document.createElement("div");
-  newTodoItem.classList.add("todo-item");
+  newTodoItem.setAttribute("id", "task");
 
   const checkBtn = document.createElement("button");
   checkBtn.innerHTML = `<i class="fa-solid fa-check fa-lg"></i>`;
@@ -34,25 +24,32 @@ function addToDo(e) {
 
   const newItem = document.createElement("li");
   newItem.classList.add("item");
-  newItem.innerText = `hei`;
+  newItem.innerText = inputBar.value;
   newTodoItem.appendChild(newItem);
 
   const trashBtn = document.createElement("button");
-  trashBtn.setAttribute("id", "trash");
+  trashBtn.classList.add("trash");
   trashBtn.innerHTML = `<i class="fa-regular fa-trash-can fa-lg"></i>`;
   newTodoItem.appendChild(trashBtn);
 
+  //If the input is empty don't add a task
   list.appendChild(newTodoItem);
+  if (newItem.innerText === "") {
+    newTodoItem.remove();
+  }
+
+  inputBar.value = "";
 }
 
 function deleteItem(e) {
-  // const deleteItem = document.getElementById("trash").closest("div");
-  // deleteItem.remove();
-  // console.log(deleteItem);
-  console.log(e);
+  const item = e.target;
+  if (item.classList[0] === "trash") {
+    const task = item.parentElement;
+    task.remove();
+  }
 }
 
-undo.addEventListener("click", function () {
-  const undoItem = removedItems.pop();
-  list.appendChild(undoItem);
-});
+// undo.addEventListener("click", function () {
+//   const undoItem = removedItems.pop();
+//   list.appendChild(undoItem);
+// });
